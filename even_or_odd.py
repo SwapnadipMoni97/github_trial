57e1a783917b7ddd34d5bc88d1e2862d079155ba
SwapnadipMoni97 <69101598+SwapnadipMoni97@users.noreply.github.com>
2025-03-13 06:50:54 +0530

    even_or_odd

---
+++ even_or_odd.py
@@ -0,0 +1,11 @@
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Example usage
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    result = check_even_or_odd(num)
+    print(f"The number {num} is {result}.")

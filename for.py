db7decee36fd767e4030d8734de9624bbdd53a96
SwapnadipMoni97 <69101598+SwapnadipMoni97@users.noreply.github.com>
2025-03-29 20:52:05 +0530

    adding other files

---
+++ for.py
@@ -0,0 +1,38 @@
+# nums = [2, 7, 11, 17, 8]
+
+# # total_sum = 0
+
+# def array_sum_using_for_loop(list):
+#     total_sum = 0
+#     for i in nums:
+#         total_sum += i
+#     return total_sum    
+
+# result = array_sum_using_for_loop(nums)
+
+# print(f"the sum of the given array is {result}")    
+
+######################################################################################################
+# # sum of even numbers from 1 to 10 
+# even_sum = 0
+
+# for i in range(11):
+#     if i % 2 == 0:
+#         even_sum += i
+#     else:
+#         pass
+    
+# print(f"the deisred sum of even numbers is {even_sum}")    
+# ###########################################################################################################
+# count odd numbers between 1 and 20
+odd_count = 0
+
+for i in range(21):
+    if i % 2 != 0:
+        odd_count += 1
+    else :
+        pass
+   
+print(f"The count of odd numbers is {odd_count}")     
+
+###############################################################################################################   
--- while.py
+++ while.py
@@ -1,3 +1,4 @@
+####################################################################################################
 # def sum_of_numbers(numbers):
 #     total_sum = 0
 #     index = 0
@@ -11,7 +12,7 @@
 # numbers = [1, 2, 3, 4, 5]
 # result = sum_of_numbers(numbers)
 # print("The sum of the numbers in the list is:", result)
-#------------------------------------------------------------------------------------------
+#####################################################################################################
 #use while loops to iterate through a list
 # def sum_of_numbers_of_a_list(numbers):
 #     """ This function takes in a list of numbers and returns the sum of the numbers"""
@@ -24,4 +25,33 @@
 
 # result = sum_of_numbers_of_a_list([3, 5, 8, 14, 25])
 # print(f"the sum of the numbers in the list is : {result}")
-#------------------------------------------------------------------------------------------
+#####################################################################################################
+#print sum of even numbers from 1 to 10
+
+# even_sum = 0
+# i = 0
+# while i <= 10 :
+#     if i%2==0:
+#         even_sum += i
+#     else:
+#         pass
+#     i += 1         
+    
+# print(f"the desired sum is {even_sum}") 
+######################################################################################################
+#count odd numbers between 1 to 20
+
+odd_count = 0
+
+i = 0
+
+while i <= 20 :
+    if i % 2 != 0:
+        odd_count += 1
+    else:
+        pass
+    i += 1
+    
+print(f"The odd count is {odd_count}") 
+
+########################################################################################################      
